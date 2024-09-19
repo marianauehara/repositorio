a2cc8589da499ca7c131dea928916458b1b4f827
marianauehara <00001131866708sp@al.educacao.sp.gov.br>
2024-09-19 11:40:45 -0300

    Add files via upload

---
+++ exercicio1.py
@@ -0,0 +1,4 @@
+while True: #loop infinito (repete infinitamente)
+    dolar =float(input("DIGITE O VALOR EM DÓLARES: "))
+    valor_reais = dolar *4.95
+    print("O VALOR EM REAIS É: ", round(valor_reais, 2))
